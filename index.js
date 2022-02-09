a66ccfc758d0acd8023d362ee2a0bdbb444282b1
ejnksj <94329300+ejnksj@users.noreply.github.com>
2022-02-09 16:37:23 +0900

    22-02-09 start

---
+++ index.js
@@ -0,0 +1,27 @@
+const puppeteer = require('puppeteer');
+function delay(ms) {
+  return new Promise(function(resolve) {
+    setTimeout(resolve, ms)
+  });
+}
+const twitchUserName = "tinatu00";
+const twitchUserPassword = "twip12!@twip12!@";
+const twitchLoginButton = '#root > div > div.scrollable-area > div.simplebar-scroll-content > div > div > div > div.Layout-sc-nxg1ff-0.eFpRTs > form > div > div:nth-child(3) > button';
+const autoBrowser = async () => {
+  const browser = await puppeteer.launch({headless: false});
+  const page = await browser.newPage();
+  await page.goto('https://twip.kr');
+  await page.click("#login-button-area a.lbtn.watcher");
+
+  await delay(3500);
+
+  await page.click('input[id=login-username]');
+  await page.type('[id=login-username]', twitchUserName);
+  await page.click('input[id=password-input]');
+  await page.type('[id=password-input]', twitchUserPassword);
+  await page.click(twitchLoginButton);
+
+}
+
+
+autoBrowser();
