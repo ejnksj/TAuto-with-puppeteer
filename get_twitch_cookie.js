a925b56041323bdb1941105ebe573ff1b038c572
ejnksj <94329300+ejnksj@users.noreply.github.com>
2022-05-20 09:30:15 +0900

    Add files via upload

--- get_twitch_cookie.js
+++ get_twitch_cookie.js
@@ -5,20 +5,22 @@ function delay(ms) {
     setTimeout(resolve, ms)
   });
 }
-const twitchUserName = "ID";
-const twitchUserPassword = "PW";
-
+const twitchUserName = "twitch ID";
+const twitchUserPassword = "twitch PW";
+const twipURL = 'https://twip.kr';
 const twitchLoginButton = '#root > div > div.scrollable-area > div.simplebar-scroll-content > div > div > div > div:nth-child(3) > form > div > div:nth-child(3) > button';
 
 (async () => {
   const browser = await puppeteer.launch(
     {headless: false, executablePath: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe'},
-    {args: ['--user-data-dir=C:\\Users\\user\\AppData\\Local\\Google\\Chrome\\User Data']}
+    {args: ['--user-data-dir=C:\\Users\\tehrb\\AppData\\Local\\Google\\Chrome\\User Data']}
   );
-  const page = await browser.newPage();
+  //const page = await browser.newPage();
+  let pages = await browser.pages();
+  let page = pages[0];
 
   await page.setViewport({width: 800, height: 600});
-  await page.goto('https://twip.kr/');
+  await page.goto(twipURL);
 
   await page.click("#login-button-area a.lbtn.watcher");
   await page.waitForSelector(twitchLoginButton,{visible: true});
@@ -29,7 +31,7 @@ const twitchLoginButton = '#root > div > div.scrollable-area > div.simplebar-scr
   await page.click('input[id=password-input]');
   await page.type('[id=password-input]', twitchUserPassword);
   await page.click(twitchLoginButton);
-//  await page.waitForSelector('#nav > a:nth-child(5)',{visible: true});
+  await page.waitForSelector('#nav > a:nth-child(5)',{visible: true});
 
   await delay(8000);
 
@@ -40,4 +42,5 @@ const twitchLoginButton = '#root > div > div.scrollable-area > div.simplebar-scr
   fs.writeFileSync('cookies.json', cookieJson)
 
   console.log("GET_COOKIES End");
+  await page.close();
 })();
--- twiptest_autologin_loop.js
+++ twiptest_autologin_loop.js
@@ -5,18 +5,20 @@ function delay(ms) {
     setTimeout(resolve, ms)
   });
 }
-const twitchUserName = "ID";
-const twitchUserPassword = "PW";
-const twitchAlertboxURL = 'twipAlertboxURL';
+const twitchUserName = "twitch ID";
+const twipURL = 'https://twip.kr';
+const twipAlertboxURL = twipURL+'/widgets/alertbox/a6ypn1o857';
 
 //main function
 (async () => {
   //open page, goto twip.kr
   const browser = await puppeteer.launch(
     {headless: true, executablePath: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe'},
-    {args: [/*'--window-size=1388,768', '--remote-debugging-port=21222', */'--user-data-dir=C:\\Users\\user\\AppData\\Local\\Google\\Chrome\\User Data']}
+    {args: ['--user-data-dir=C:\\Users\\tehrb\\AppData\\Local\\Google\\Chrome\\User Data']}
   );
-  const alertboxPage = await browser.newPage();
+  let pages = await browser.pages();
+  const alertboxPage = pages[0];
+//  const alertboxPage = await browser.newPage();
   const page = await browser.newPage();
 
   ///////////set cookies
@@ -26,37 +28,70 @@ const twitchAlertboxURL = 'twipAlertboxURL';
   await page.setCookie(...deserializedCookies);
   ///////////
 
-  await alertboxPage.goto(twitchAlertboxURL);
+  await alertboxPage.goto(twipAlertboxURL);
 
   await page.setViewport({width: 800, height: 600});
-  await page.goto('https://twip.kr/');
+  await page.goto(twipURL);
   await delay(1500);
 
-  /////////////LOOP
-  while(1){
   //goto donation page(self)
   await page.goto(page.url()+twitchUserName+'/');
+  /////////////LOOP
+  while(1){
   //get current cash
   const nowCashSelector = 'body > nav > div.maybe-wrapped-in-dropdown > div > a > span.now-cash';  //  'body > nav:nth-child(2) > div.maybe-wrapped-in-dropdown > div > a > span.now-cash'
   var tStamp = new Date().getTime();
   var verifyTime = tStamp;
-  await page.waitForSelector(nowCashSelector);
+  var now = new Date();
+  try{
+  await page.waitForSelector(nowCashSelector, {timeout: 5000});
+} catch (e) {
+    page.screenshot({path: './screenshots/pageerror'+verifyTime+'.png', fullPage: true});
+    console.log('↓screen captured(pageerror'+verifyTime+'.png)');
+    console.log("*보유 캐시 정보를 찾을 수 없습니다. 스크린샷을 확인하세요. / "+ now);
+    await delay(295000);
+    page.reload();
+    continue;
+  }
+
   let cashElement = await page.$(nowCashSelector);
   let cashBefore = await page.evaluate(el => el.textContent, cashElement);
   cashBefore = Number(cashBefore.replace(/,/g, ""));
+  //log error and goto wait when cash is not enough
+  var now = new Date();
+  if (cashBefore < 1000) {
+    console.log("테스트계정 잔여캐시 부족(" + cashBefore + ") / " + now);
+    page.screenshot({path: './screenshots/casherror'+verifyTime+'.png', fullPage: true});
+    console.log('↓screen captured(casherror'+verifyTime+'.png)');
+  } else {
+
   //console.log("cashBefore activated");
   //excute message donation
+  try{
   await page.waitForSelector('#donation-text-textarea');
+  await delay(3000);
   await page.click('#donation-text-textarea');
   await page.type('#donation-text-textarea', "MessageDonationTest_via_Puppeteer_"+ verifyTime);
+  await delay(1000);
   await page.click('#make-donate-btn');
+} catch (e) {
+  console.log("후원페이지 에러: "+e);
+}
+
   //console.log("donation activated");
   //get current cash(after donation)
-  await delay(3000);
+  await delay(10000);  //2시간 이상 동작 시, 후원이 될 때까지 지연이 5초 이상 걸리는 현상이 있음
   await page.waitForSelector(nowCashSelector);
   cashElement = await page.$(nowCashSelector);
   let cashAfter = await page.evaluate(el => el.textContent, cashElement);
   cashAfter = Number(cashAfter.replace(/,/g, ""));
+  let isCashPayed = (cashBefore === (cashAfter+1000));
+  //take screenshot if cash is not payed
+  if (isCashPayed == false) {
+    page.screenshot({path: './screenshots/casherror'+verifyTime+'.png', fullPage: true});
+    console.log('↓screen captured(casherror'+verifyTime+'.png)');
+  }
+
 
   //focus to overlay tab, and get donation text
   alertboxPage.bringToFront();
@@ -66,16 +101,20 @@ const twitchAlertboxURL = 'twipAlertboxURL';
   let compareText = await alertboxPage.$(compareTextSelector);
   let compareText_Val = await alertboxPage.evaluate(el => el.textContent, compareText);
   let isDonated = (compareText_Val === ("MessageDonationTest_via_Puppeteer_"+ verifyTime));
-  let isCashPayed = (cashBefore === (cashAfter+1000));
+
   let testBool = isDonated && isCashPayed;
 
   var now = new Date();
   console.log(
-    "메시지후원 " + testBool + " / " + now
+    "메시지후원 " + testBool + " / 테스트계정 잔여캐시: " + cashAfter + " / " + now
   );
-await delay(6000);
-page.bringToFront();
-await delay(1800000);
-}
+
+  } //(cashBefore < 1000)의 else문 닫는 괄호
+
+  await delay(6000);
+  page.bringToFront();
+  await delay(277500);
+  page.reload();
+  } //while(1) 닫는 괄호
 
 })();
